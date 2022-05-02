1b3b03e0e501e18831d2f5753380ee9a50588381
Harshil Shiyani <95012680+harshilkumar@users.noreply.github.com>
2022-05-02 04:54:23 -0700

    Add files via upload

---
+++ Otp.java
@@ -0,0 +1,8 @@
+class Otp{
+	public static void main(String[] args) {
+		double a=Math.random()*1000000;
+		int otp=(int)a;
+		System.out.println("Your Otp is generated successfully "+otp);
+		
+	}
+}
